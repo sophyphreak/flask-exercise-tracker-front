63b6245a69e09003ae734615248d2b288897dfb2
Andrew Horn <arhorn@smcm.edu>
2020-04-07 14:38:12 -0500

    ran prettier

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,62 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import "./style.css"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <div className="container">
+    <h1>Exercise tracker</h1>
+    <h3>Created by Andrew Horn</h3>
+    <form
+      action="https://flask-exercise-tracker.andrew-horn-portfolio.life/api/exercise/new-user"
+      method="post"
+    >
+      <h3>Create a New User</h3>
+      <p>
+        <code>POST /api/exercise/new-user</code>
+      </p>
+      <input id="uname" type="text" name="username" placeholder="username" />
+      <input type="submit" value="Submit" />
+    </form>
+    <form
+      action="https://flask-exercise-tracker.andrew-horn-portfolio.life/api/exercise/add"
+      method="post"
+    >
+      <h3>Add exercises</h3>
+      <p>
+        <code>POST /api/exercise/add</code>
+      </p>
+      <input id="uid" type="text" name="userId" placeholder="userId*" />
+      <input
+        id="desc"
+        type="text"
+        name="description"
+        placeholder="description*"
+      />
+      <input
+        id="dur"
+        type="text"
+        name="duration"
+        placeholder="duration* (mins.)"
+      />
+      <input id="dat" type="text" name="date" placeholder="date (yyyy-mm-dd)" />
+      <input type="submit" value="Submit" />
+    </form>
+    <p>
+      <strong>GET users's exercise log: </strong>
+      <code>
+        GET{" "}
+        {
+          "https://flask-exercise-tracker.andrew-horn-portfolio.life/api/exercise/log?{userId}[&from][&to][&limit]"
+        }
+      </code>
+    </p>
+    <p>
+      <strong>{}</strong> = required, <strong>[ ]</strong> = optional
+    </p>
+    <p>
+      <strong>from, to</strong> = dates (yyyy-mm-dd); <strong>limit</strong> =
+      number
+    </p>
+  </div>
 )
 
 export default IndexPage
